c2a3a54ec34659bfc52bef4394bcee57855fede2
Manish Tripathy <manisht@thoughtworks.com>
2018-03-18 12:48:30 +0530

    Displaying child components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import { render } from 'react-dom'
-import {SkiDayCount} from './components/SkiDayCount'
+import {SkiDayList} from './components/SkiDayList'
 
 //Sometimes we get an error which says React is not defined. Adding React to the window solves the problem
 window.React = React;
 
 render(
-	<SkiDayCount total={50}
-				powder={20}
-				backcountry={10}
-				goal={100}/>,
+	<SkiDayList days={
+		[
+			{
+				resort:"Square Valley",
+				date: new Date("1/12/2018"),
+				powder:true,
+				backcountry:false
+			},
+			{
+				resort:"Kirkwood",
+				date: new Date("12/12/2016"),
+				powder:false,
+				backcountry:false
+			},
+			{
+				resort:"Shera Valley",
+				date: new Date("4/9/2017"),
+				powder:false,
+				backcountry:true
+			}
+		]	
+	}/>,
 	document.getElementById('react-container')
 )
