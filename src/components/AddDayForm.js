2b394192dc62b0d3e871541774e6ac0df2e58411
Manish Tripathy <manisht@thoughtworks.com>
2018-03-18 17:56:04 +0530

    Using refs in class components

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -1,26 +1,47 @@
 import { PropTypes, Component } from 'react'
 
 export class AddDayForm extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.submit = this.submit.bind(this) //We need to bind this so that this can be used inside the function
+    }
+    
+    submit(e) {
+        e.preventDefault();
+        console.log('resort', this.refs.resort.value)
+        console.log('date', this.refs.date.value)
+        console.log('powder', this.refs.powder.checked)
+        console.log('backcountry', this.refs.backcountry.checked)
+    }
+
     render() {
         const {resort, date, powder, backcountry } = this.props
         return (
-            <form className="add-day">
+            <form onSubmit={this.submit} className="add-day">
                 <label htmlFor="resort">Resort Name</label>
                 <input id="resort" type="text" required
-                defaultValue={resort} />
+                defaultValue={resort} 
+                ref="resort"/>
                 
                 <label htmlFor="date">Date</label>
                 <input id="date" type="text" required 
-                defaultValue={date}/>
+                defaultValue={date}
+                ref="date"/>
                 
                 <div>
-                    <input id="powder" type="checkbox" defaultChecked={powder}/>
+                    <input id="powder" type="checkbox" 
+                    defaultChecked={powder} 
+                    ref = "powder"/>
                     <label htmlFor="powder">Powder Day</label>
                 </div>
                 <div>
-                    <input id="backcountry" type="checkbox" defaultChecked={backcountry}/>
+                    <input id="backcountry" type="checkbox" 
+                    defaultChecked={backcountry}
+                    ref="backcountry"/>
                     <label htmlFor="backcountry">Backcountry day</label>
                 </div>
+                <button>Add day</button>
             </form>
         )
     }
