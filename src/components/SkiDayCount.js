d0dc3b084dfdc280309e9f2052ae762ad4e07665
Manish Tripathy <manisht@thoughtworks.com>
2018-03-18 10:43:06 +0530

    Refactored SkiDayCount component to use Stateless functional component syle

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,34 +1,33 @@
 import {Component} from 'react'
 import '../stylesheets/ui.scss'
 
-export class SkiDayCount extends Component{
-    percentToDecimal(decimal) {
-        return ((decimal*100) + '%')
-    }
-    calcGoalProgress(total,goal) {
-        return this.percentToDecimal(total/goal);
-    }
-    render() {
-        return (
-            <div>
-                <div className="ski-day-count">
-                    <div className="total-days">
-                        <span>{this.props.total}</span>  
-                        <span>days </span>
-                    </div>
-                    <div className="powder-days">
-                        <span>{this.props.powder}</span>  
-                        <span>days </span>
-                    </div>
-                    <div className="backcountry-days">
-                        <span>{this.props.backcountry}</span>  
-                        <span>days </span>
-                    </div>
-                    <div>
-                        <span>{this.calcGoalProgress(this.props.total,this.props.goal)}</span>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+const percentToDecimal = (decimal) => {
+   return ((decimal*100) + '%')
 }
+
+const calcGoalProgress = (total,goal)=> {
+    return percentToDecimal(total/goal)
+}
+
+
+export const SkiDayCount = (props) => (
+    <div>
+        <div className="ski-day-count">
+            <div className="total-days">
+                <span>{props.total}</span>  
+                <span>days </span>
+            </div>
+            <div className="powder-days">
+                <span>{props.powder}</span>  
+                <span>days </span>
+            </div>
+            <div className="backcountry-days">
+                <span>{props.backcountry}</span>  
+                <span>days </span>
+            </div>
+            <div>
+                <span>{calcGoalProgress(props.total,props.goal)}</span>
+            </div>
+        </div>
+    </div>
+)
