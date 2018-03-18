7fe610c91ae9e7ca2ed73c67319e268ef557df90
Manish Tripathy <manisht@thoughtworks.com>
2018-03-18 10:11:54 +0530

    Specific import added for React.Component

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import {Component} from 'react'
 import '../stylesheets/ui.scss'
 
-export class SkiDayCount extends React.Component{
+export class SkiDayCount extends Component{
     percentToDecimal(decimal) {
         return ((decimal*100) + '%')
     }
